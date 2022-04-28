24f8d2fe5c07177def5f1abb5e93a73e06879a43
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-04-28 12:01:03 +0600

    objects practices

---
+++ fundamentals/loops.js
@@ -0,0 +1,4 @@
+let = 0;
+for(let i = 0; i < 10; i++){
+    console.log(i , "Hello World");
+}
---
+++ fundamentals/objects.js
@@ -0,0 +1,18 @@
+let Person = {
+    name: "John",
+    age: 30,
+    isMarried: false,
+    hobbies: ["Sports", "Cooking"],
+    wearShirt: true,
+    shirt:{
+        color: "red",
+        size:{
+            small: "S",
+            medium: "M",
+            large: "L"
+        },
+        quality: "good",
+        price: "1000Taka"
+    }
+}
+console.log(Person.shirt);
