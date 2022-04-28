f51851d868b230649d07e7321d7b9cfde5179d1b
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-04-28 11:47:40 +0600

    for of loop practices

---
+++ for_of_loops.js
@@ -0,0 +1,5 @@
+// The for of loop is a more modern way to loop through arrays and objects.
+let theCountry = ['USA', 'Germany', 'Italy', 'France', 'Japan'];
+for (let country of theCountry) {
+    console.log(country);
+};
