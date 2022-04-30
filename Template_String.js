d3a4dce80a09d018f7a91e597f527793facbdf67
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-04-30 20:18:17 +0600

    js modules and template string practices

---
+++ Template_String.js
@@ -0,0 +1,5 @@
+let firstName = "Gaus Al Munir";
+let lastName = "Tushar";
+let fullName = `${firstName} ${lastName}`;
+
+export { firstName, lastName, fullName };
---
+++ modules.js
@@ -0,0 +1,4 @@
+// Module: 
+// Module import and export:
+import { firstName, lastName, fullName } from './Template_String.js';
+console.log(firstName);
