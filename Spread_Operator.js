9d83928a56724567c5b8ceac0b8ee5f789ce41b8
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-03 14:57:20 +0600

    spread opertors pratices

--- Spread_Operator.js
+++ Spread_Operator.js
@@ -1,6 +1,9 @@
 // tree dots // Spread Operator;
-let poorCountry = ["India", "Pakistan", "Bangladesh", "Nepal", "Bhutan", "Sri Lanka"];
+/* let poorCountry = ["India", "Pakistan", "Bangladesh", "Nepal", "Bhutan", "Sri Lanka"];
 let richCountry = [...poorCountry,"USA", "Canada", "Australia", "New Zealand", "Singapore", "Hong Kong"];
+console.log(richCountry); */
+let poorCountry = ["India", "Pakistan", "Bangladesh", "Nepal", "Bhutan", "Sri Lanka"];
+let richCountry = [...poorCountry , "USA", "Canada", "Australia", "New Zealand", "Singapore", "Hong Kong"];
 console.log(richCountry);
 
 // without spread operator
---
+++ main.js
@@ -0,0 +1,7 @@
+// console.log("hello world");
+// Strcit Mode in JavaScript:
+"use strict"
+function myName (){
+    name = "Tushar";
+}
+myName();   
