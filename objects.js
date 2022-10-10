4a047d8134b34a3e3b5198fb42b12f03df374aee
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-10 16:05:06 +0600

    js objects practices

---
+++ objects.js
@@ -0,0 +1,16 @@
+let studentDetails = {
+    tushar:{
+        fullname: "Tushar",
+        age: 20,
+        isMarried: false,
+        hobbies: ["Sports", "Cooking"],
+    } ,
+    gaus:{
+        fullname: "Gaus",
+        age: 20,
+        isMarried: false,
+        hobbies: ["Sports", "Cooking"],
+    }
+}
+
+console.log(studentDetails.tushar.fullname);
