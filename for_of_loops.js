55c05d3025fc6376b36ec6d039be6286df8232c8
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-10 17:36:05 +0600

    for in loop and for of loop practices

--- for_of_loops.js
+++ for_of_loops.js
@@ -1,5 +1,17 @@
 // The for of loop is a more modern way to loop through arrays and objects.
-let theCountry = ['USA', 'Germany', 'Italy', 'France', 'Japan'];
+/* let theCountry = ['USA', 'Germany', 'Italy', 'France', 'Japan'];
 for (let country of theCountry) {
     console.log(country);
-};
+}; */
+
+let myDetails ={
+    name: "Tushar",
+    age: 20,
+    isMarried: false,
+    hobbies: ["Sports", "Cooking"],
+}
+
+for( let props in myDetails){
+    // console.log(props);
+    console.log(`${props} : ${myDetails[props]}`);
+}
